a4309b862a9d74d7bdd82391af8e6cea7dffb76a
Debanjana <debanjana.9806@gmail.com>
2022-12-08 20:55:11 -0800

    submit

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
 //your code here
-
+function swapTheme() {
+    var element = document.getElementById('app');
+    element.classList.toggle("night");
+    var btn= document.getElementById('swap');
+    btn.classList.toggle("button_night");
+ }
